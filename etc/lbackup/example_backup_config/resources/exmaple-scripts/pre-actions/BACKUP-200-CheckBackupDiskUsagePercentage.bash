e10deccd58830583daab7505d498de14bb1c0540
Henri Shustak <henri.shustak@gmail.com>
2011-05-19 16:28:14 +1200

    minor changes to output formatting.

--- etc/lbackup/example_backup_config/resources/exmaple-scripts/pre-actions/BACKUP-200-CheckBackupDiskUsagePercentage.bash
+++ etc/lbackup/example_backup_config/resources/exmaple-scripts/pre-actions/BACKUP-200-CheckBackupDiskUsagePercentage.bash
@@ -10,7 +10,7 @@ PATH=/usr/local/bin:/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin
 ##  Ensure Disk Usage is Bleow a Set Percentage ##
 ##      	         (C)2008                    ##
 ##						                        ##
-##	              Version 0.0.2 	            ##
+##	              Version 0.0.3 	            ##
 ##                                              ##
 ##          Developed by Henri Shustak          ##
 ##                                              ##
@@ -59,8 +59,8 @@ backupDestVolume_diskusage_percentage=0
 
 ## Preflight check
 if ! [ -d "${backupDestVolume}" ]  ; then
-    echo "                ERROR! : Unable to locate the backup volume. As such, unable to" | tee -ai $logFile
-    echo "                         calculate percentage disk usage on the backup destination volume." | tee -ai $logFile
+    echo "    ERROR! : Unable to locate the backup volume. As such, unable to" | tee -ai $logFile
+    echo "             calculate percentage disk usage on the backup destination volume." | tee -ai $logFile
     exit ${SCRIPT_HALT}
 fi
 
@@ -69,8 +69,8 @@ fi
 function calculate_disk_usage_for_backup_destination_volume {
     backupDestVolume_diskusage_percentage=`df -h | grep "${backupDestVolume}" | awk '{print $5}' | awk -F "%" '{print $1}'`
     if [ $? != 0 ] ; then
-        echo "                ERROR! : Unable to calculate percentage disk usage on the backup destination volume." | tee -ai $logFile
-        echo "                         ${backupDestVolume}" | tee -ai $logFile
+        echo "    ERROR! : Unable to calculate percentage disk usage on the backup destination volume." | tee -ai $logFile
+        echo "             ${backupDestVolume}" | tee -ai $logFile
         exit ${SCRIPT_HALT}
     fi
 }
@@ -84,13 +84,13 @@ calculate_disk_usage_for_backup_destination_volume
 # return approriate value based upon the disk utilization of the backup volume
 if [ ${backupDestVolume_diskusage_percentage} -ge $stop_backup_if_disk_usage_is_at_or_above ] ; then
      echo ""
-     echo "                ERROR! : Disk usage on the backup destination volume is too high." | tee -ai $logFile
-     echo "                         Backup volume disk utilization is : ${backupDestVolume_diskusage_percentage}%" | tee -ai $logFile
+     echo "    ERROR! : Disk usage on the backup destination volume is too high." | tee -ai $logFile
+     echo "             Backup volume disk utilization is : ${backupDestVolume_diskusage_percentage}%" | tee -ai $logFile
      echo ""
      exit ${SCRIPT_HALT}
 else
     if [ "${report_disk_utilization_of_the_backup_volume}" == "YES" ] ; then
-        echo "                Backup volume disk utilization is : ${backupDestVolume_diskusage_percentage}%" | tee -ai $logFile
+        echo "    Backup volume disk utilization is : ${backupDestVolume_diskusage_percentage}%" | tee -ai $logFile
     fi
     exit ${SCRIPT_SUCCESS}
 fi
